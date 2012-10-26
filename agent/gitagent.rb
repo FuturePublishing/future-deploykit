cf8304df21b31550cb771e22a179d5178c9b8861
John Hawkes-Reed <john.hawkes-reed@futurenet.com>
2012-10-26 12:51:36 +0100

    (i) Much more logging for the purposes of internal deploy audit. (ii) Optional display of most recent 'n' tags for long-lived projects.

--- agent/gitagent.rb
+++ agent/gitagent.rb
@@ -7,16 +7,20 @@ module MCollective
       require 'syslog'
       require 'socket'
       
-      metadata :name        => "Git agent",
-        :description => "Git agent",
-        :author      => "JHR",
-        :license     => "BSD",
-        :version     => "0.4",
-        :url         => "https://github.com/futureus/future-deploykit",
-        :timeout     => 60
+      metadata  :name        => "Git agent",
+                :description => "Git agent",
+                :author      => "JHR",
+                :license     => "BSD",
+                :version     => "0.5",
+                :url         => "https://github.com/futureus/deploybot-40k",
+                :timeout     => 60
 
       activate_when do
         File.exists?("/etc/facts.d/facts.yaml")
+      end
+
+      def create_id
+        request.data[:request_id] = Digest::MD5.hexdigest request.data[:repo] + " " + request.data[:tag] + " " + "#{Time.now.to_i}"
       end
 
       action "git_tag" do
@@ -25,29 +29,43 @@ module MCollective
         rconfig = YAML.load_file("/etc/facts.d/facts.yaml")
         lrepo = rconfig["repo_#{request[:repo]}"]
         reply[:lrep] = lrepo
-        reply[:status] = run("/usr/bin/git tag", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
-        reply[:tstatus] = reply[:status]
-        reply[:tout] = reply[:out]
 
-        reply[:status] = run("/usr/bin/git branch -a", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
-        reply[:bstatus] = reply[:status]
-        reply[:bout] = reply[:out]
+        count = request.data.fetch(:count) { nil }
+        if count
+          validate :count, Fixnum
+          reply[:status] = run("/usr/bin/git for-each-ref --format '%(refname)' --sort=-taggerdate --count=#{count} refs/tags", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
+          reply[:tstatus] = reply[:status]
+          reply[:tout] = reply[:out]
+
+          reply[:status] = run("/usr/bin/git for-each-ref --format '%(refname)' --sort=-committerdate --count=#{count} refs/", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
+          reply[:bstatus] = reply[:status]
+          reply[:bout] = reply[:out]
+        else
+          reply[:status] = run("/usr/bin/git tag", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
+          reply[:tstatus] = reply[:status]
+          reply[:tout] = reply[:out]
+
+          reply[:status] = run("/usr/bin/git branch -a", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
+          reply[:bstatus] = reply[:status]
+          reply[:bout] = reply[:out]
+        end
       end
   
       action "git_checkout" do
         validate :repo, String
         validate :tag, String
 
+        create_id if request.data[:request_id] == nil
+
         rconfig = YAML.load_file("/etc/facts.d/facts.yaml")
         sconfig = YAML.load_file("/etc/git-agent/git-agent.yaml")
         host_name = Socket::gethostname
 
-        Syslog.open('git-agent', Syslog::LOG_CONS, Syslog::LOG_DAEMON)
         stompconnector = sconfig["rserver"] + "?" + sconfig["stomp-options"]
 
         sclient = Stomp::Client.new(stompconnector)
         if sclient.nil?
-          Syslog.info("Can't connect to stomp server.")
+          Log.info("Can't connect to stomp server.")
         end
         report_topic = sconfig["report-topic"]
 
@@ -61,6 +79,10 @@ module MCollective
         reply[:lsit] = lsite
         reply[:trub1] = precmd
         reply[:trub2] = postcmd
+
+
+        deploylog = "MC gitagent deploy tag #{request[:tag]} on #{Time.now}"
+        Log.info(deploylog)
         
         if File.exists?(precmd) and File.executable?(precmd)
           reply[:status] = run("#{precmd} -t #{request[:tag]}", :stdout => :out, :stderr => :err, :cwd => wdir, :chomp => true)
@@ -68,18 +90,26 @@ module MCollective
           reply[:prout] = reply[:out]
           reply[:prerr] = reply[:err]
 
+          deploylog = "Pre-deploy status: #{reply[:status]} stdout: #{reply[:out]} stderr: #{reply[:err]}"
+          Log.info(deploylog)
+
           if  reply[:prstat] == 0
             reply[:status] = run("/bin/su - www-data -c \"cd #{lrepo} && /usr/bin/git --work-tree=#{lsite} checkout -f #{request[:tag]}\"", :stdout => :out, :stderr => :err, :cwd => lrepo, :chomp => true)
             reply[:dstat] = reply[:status]
             reply[:dout] = reply[:out]
             reply[:derr] = reply[:err]
 
+            deploylog = "git-command status: #{reply[:status]} stdout: #{reply[:out]} stderr: #{reply[:err]}"
+            Log.info(deploylog)
+
             if reply[:dstat] == 0
               if File.exists?(postcmd) and File.executable?(postcmd)
                 reply[:status] = run("#{postcmd} -t #{request[:tag]}", :stdout => :out, :stderr => :err, :cwd => wdir, :chomp => true)
+                deploylog = "Post-deploy status: #{reply[:status]} stdout: #{reply[:out]} stderr: #{reply[:err]}"
+                Log.info(deploylog)
                 if sclient
-                  eventdetail = "git-agent on #{host_name} checked out tag #{request[:tag]} from repo #{lrepo} to target #{lsite}"
-                  Syslog.info(eventdetail)
+                  eventdetail = "git-agent on #{host_name} checked out tag #{request[:tag]} from repo #{lrepo} to target #{lsite} request id #{request[:request_id]}"
+                  Log.info(eventdetail)
                   sclient.publish("/topic/#{report_topic}",eventdetail, {:subject => "Talking to eventbot"})
                 end
               else
@@ -94,7 +124,6 @@ module MCollective
           reply[:prout] = ""
           reply[:prerr] = "File '#{precmd}' does not exist or is not executable"
         end
-        Syslog.close
       end
     end
   end
